c99d5abea323edcdcb3dd132ffa4ab7bb6f14c93
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-04-29 15:28:00 -0500

    Convert all scripts to use file-scoped namespaces

--- Godot.Logging/FormatRule.cs
+++ Godot.Logging/FormatRule.cs
@@ -1,55 +1,54 @@
 using System.Text;
 
-namespace Godot.Logging
+namespace Godot.Logging;
+
+public static class ColorExtensions
 {
-    public static class ColorExtensions
+    public static string ToHexArgb(this Color color)
     {
-        public static string ToHexArgb(this Color color)
-        {
-            var builder = new StringBuilder();
-            builder.Append("#");
-            builder.Append(color.A8.ToString("X2"));
-            builder.Append(color.R8.ToString("X2"));
-            builder.Append(color.G8.ToString("X2"));
-            builder.Append(color.B8.ToString("X2"));
-            return builder.ToString();
-        }
+        var builder = new StringBuilder();
+        builder.Append("#");
+        builder.Append(color.A8.ToString("X2"));
+        builder.Append(color.R8.ToString("X2"));
+        builder.Append(color.G8.ToString("X2"));
+        builder.Append(color.B8.ToString("X2"));
+        return builder.ToString();
+    }
+}
+
+public class FormatRule
+{
+    /// <summary>
+    /// Color to associate with the log event if the target supports color.
+    /// </summary>
+    public Color TextColor
+    {
+        get;
+        set;
+    } = Colors.White;
+
+    /// <summary>
+    /// Text code indicating the format to use to write the log event.
+    /// </summary>
+    public string FormatText
+    {
+        get;
+        set;
     }
 
-    public class FormatRule
+    /// <summary>
+    /// Log level to associate the Format Rule with.
+    /// </summary>
+    public LogLevel FormatLogLevel
     {
-        /// <summary>
-        /// Color to associate with the log event if the target supports color.
-        /// </summary>
-        public Color TextColor
-        {
-            get;
-            set;
-        } = Colors.White;
+        get;
+        set;
+    }
 
-        /// <summary>
-        /// Text code indicating the format to use to write the log event.
-        /// </summary>
-        public string FormatText
-        {
-            get;
-            set;
-        }
-
-        /// <summary>
-        /// Log level to associate the Format Rule with.
-        /// </summary>
-        public LogLevel FormatLogLevel
-        {
-            get;
-            set;
-        }
-
-        /// <summary>
-        ///  Default constructor.
-        /// </summary>
-        public FormatRule()
-        {
-        }
+    /// <summary>
+    ///  Default constructor.
+    /// </summary>
+    public FormatRule()
+    {
     }
 }
